bf90fdd347783d07c7d59c61c8b0d55d02433c7d
Ayrton De Souza <thraxbr@gmail.com>
2016-06-06 20:36:10 -0300

    Criado as funcionalidades de forma automatica

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :professors
   resources :classes
